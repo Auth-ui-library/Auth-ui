025437169d4fc7fc1542f8014b8120fcc22d8872
Jules-2MJ <146131402+2MJ-DEV@users.noreply.github.com>
2025-03-31 09:56:09 +0100

    Initialisation du projet auth-ui

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className='flex justify-center items-center min-h-screen bg-blue-900'>
+			<h1 className='text-3xl font-bold underline text-white'>
+				Template works well
+			</h1>
+		</div>
+	)
+}
+
+export default App
