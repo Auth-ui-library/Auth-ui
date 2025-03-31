1e1484efc3465912e86f40c29ddc8d9232de8775
Jules-2MJ <146131402+2MJ-DEV@users.noreply.github.com>
2025-03-31 12:35:46 +0100

    Add badel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import babel from 'vite-plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [react(), tailwindcss()],[
+    react(),
+    babel({
+      babelConfig: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+      },
+    }),
+  ],
 })
