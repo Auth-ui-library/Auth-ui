75409ba565aa481cfdc857854f4c3233b6f576e5
Jules-2MJ <146131402+2MJ-DEV@users.noreply.github.com>
2025-03-31 10:28:22 +0100

    Initialisation du projet auth-ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 	return (
 		<div className='flex justify-center items-center min-h-screen bg-blue-900'>
 			<h1 className='text-3xl font-bold underline text-white'>
-				Template works well
+				Bibliothèque des composants React
 			</h1>
 		</div>
 	)
