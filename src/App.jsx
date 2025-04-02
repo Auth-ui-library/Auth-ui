fa76ec77ed104bb71b87fcb7bb09d3e63f928987
Jules-2MJ <146131402+2MJ-DEV@users.noreply.github.com>
2025-04-02 12:16:57 +0100

    Control 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-			</Routes>
-		</Router>
-	);
+		<div className='flex justify-center items-center min-h-screen bg-blue-900'>
+			<h1 className='text-3xl font-bold underline text-white'>
+				Bienvenue sur Auth-UI
+			</h1>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/pages/About.jsx
+++
@@ -1,9 +0,0 @@
-function About() {
-    return (
-        <div>
-            <h1>À propos de nous</h1>
-        </div>
-    );
-}
-
-export default About;
